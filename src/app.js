108c2e73056c3554d258f3905f86239849a8c8a6
Aniket Gupta <aniket17112000@gmail.com>
2024-12-30 22:54:58 +0530

    feat : updated sessions

--- src/app.js
+++ src/app.js
@@ -23,7 +23,12 @@ app.use(
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: NODE_ENV === 'production' },
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      sameSite: 'none',
+    },
   }),
 );
 
